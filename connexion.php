2bbb3716f289669b4286c2aa967998fce77772fc
taplincorentin <131886831+taplincorentin@users.noreply.github.com>
2023-06-14 12:01:06 +0200

    tuto pdo

--- connexion.php
+++ connexion.php
@@ -1,10 +1,8 @@
 <?php
 try {
     $db = new PDO('mysql:host=localhost;dbname=gaulois;charset=utf8', 'root', '');
-    echo 'bravo';
 }
 catch (Exception $e) {
-    echo 'error';
     die('Erreur : ' . $e->getMessage());
 }
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php 
     require 'connexion.php';
 
+    $statement = $db->prepare('SELECT *FROM personnage');
+    $statement->execute();
+    $personnages = $statement->fetchAll();
     
+    foreach($personnages as $personnage){
+        echo $personnage['nom_personnage']."<br>";
+    }
+
+    //another example: show address from name
+    
+
+        $query = 'SELECT adresse_personnage FROM personnage WHERE nom_personnage = :personnage';
+        $statement = $db->prepare($query);
+        $statement->execute([
+            'personnage'=> 'Bonemine',
+        ]);
+        $res = $statement->fetchAll();
+        var_dump($res);
+
+ 
+
+
 ?>
